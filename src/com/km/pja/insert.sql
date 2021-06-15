76e06195df7f486de22c5a8d046ce2a8d5f1ec39
kinga-marszalkowska <kinga.marszalkowska72@gmail.com>
2021-06-15 11:28:16 +0200

    - fixed date format

--- src/com/km/pja/insert.sql
+++ src/com/km/pja/insert.sql
@@ -1,5 +1,3 @@
---todo format daty w release date
-
 INSERT INTO publisher (id, name) VALUES (DEFAULT, 'rebel');
 INSERT INTO publisher (id, name) VALUES (DEFAULT, 'Days of Wonder');
 INSERT INTO publisher (id, name) VALUES (DEFAULT, 'mattel');
@@ -24,7 +22,7 @@ INSERT INTO category (id, name) VALUES (DEFAULT, 'podróże');
 INSERT INTO game (id, "title", "publisher_id", "shortDescription", "longDescription",
 "language", "playersMin", "playersMax", "timeMin", "timeMax", "ageMin", "weight", "releaseDate")
 VALUES (DEFAULT, 'Dobble', 1, 'Gra, która rozkręci każdą imprezę!', 'Spójrz na pierwszą kartę, którą trzymasz w ręku oraz na tę, która leży na środku stołu. Znajdź na nich wspólny symbol, nazwij go i szybko pozbądź się swojego kartonika. Teraz następny! Tylko nie zwątp, każda karta łączy się z inną zawsze tylko jednym symbolem! Dobble to rozrywka absolutnie dla każdego - gra nadaje się zarówno do zabawy z dziećmi, jak i na imprezy dorosłych. 55 kolorowych kart zamkniętych jest w solidnej, metalowej puszce, dzięki czemu łatwo zabrać Dobble ze sobą i grać absolutnie wszędzie: w poczekalni u lekarza, w pociągu, na wycieczce… ',
-        'pl', 2, 8, 5, 10, 6, 0.22, 'November 23, 2020');
+        'pl', 2, 8, 5, 10, 6, 0.22, '01/01/2011');
 
 INSERT INTO author_game ("author_id", "game_id") VALUES (1,1);
 INSERT INTO author_game ("author_id", "game_id") VALUES (2,1);
@@ -40,7 +38,7 @@ INSERT INTO category_game ("category_id", "game_id") VALUES (5,1);
 INSERT INTO game (id, "title", "publisher_id", "shortDescription", "longDescription",
 "language", "playersMin", "playersMax", "timeMin", "timeMax", "ageMin", "weight", "releaseDate")
 VALUES (DEFAULT, 'Elekt', 4, 'Cesarz jest wyczerpany...', 'Elekt to taktyczna gra karciana dla 2-4 osób utrzymana w stylistyce dark fantasy. Gracze wcielają się w pretendentów do tronu mrocznego Dominatu Ebionitów. Aby zostać nowym władcą, będziecie przejmować władzę i spiskować. Będziecie przywoływać upiory i sługi ciemności, aby zasilać ich moce żarem. Najprzebieglejszy z Was zasiądzie na szczycie Wieży Dzwonów.',
-        'pl', 2, 4, 60, 90, 16, 0.5, 'November 23, 2020');
+        'pl', 2, 4, 60, 90, 16, 0.5, '23/11/2020');
 
 INSERT INTO author_game ("author_id", "game_id") VALUES (4, 2);
 INSERT INTO illustrator_game ("illustrator_id", "game_id") VALUES (1, 2);
@@ -51,7 +49,7 @@ INSERT INTO category_game ("category_id", "game_id") VALUES (2,2);
 INSERT INTO game (id, "title", "publisher_id", "shortDescription", "longDescription",
 "language", "playersMin", "playersMax", "timeMin", "timeMax", "ageMin", "weight", "releaseDate")
 VALUES (DEFAULT, 'Wsiąść do Pociągu: Europa', 2, 'Bezkonkurencyjna gra familijna z mapą Europy', 'Od urwistych wzgórz Edynburga po słoneczne nabrzeża Stambułu, z dusznych alej Pampeluny do wietrznego Berlina - Wsiąść do pociągu: Europa zabierze cię w wielką podróż przez wielkie miasta Starego Kontynentu u progu zeszłego stulecia.Czy odważysz się przebyć mroczne tunele Szwajcarii? ',
-        'pl', 2, 5, 60, 90, 8, 1.415, 'August 2019');
+        'pl', 2, 5, 60, 90, 8, 1.415, '01/08/2019');
 
 INSERT INTO category_game ("category_id", "game_id") VALUES (7,3);
 INSERT INTO category_game ("category_id", "game_id") VALUES (2,3);
