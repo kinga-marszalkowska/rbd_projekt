cf5e4a0a30a12c2e636a90ada75a7dda032b56a8
kinga-marszalkowska <kinga.marszalkowska72@gmail.com>
2021-06-19 13:32:01 +0200

    - corrected column names in select

--- src/com/km/pja/select.sql
+++ src/com/km/pja/select.sql
@@ -1,3 +1,3 @@
 -- get 1st user's order history
-SELECT order_game.id, order_game.game_id, order_game.quantity, order.user_id, order.date, order.status
-FROM order_game JOIN order ON order_game.order_id = order.id WHERE order.user_id = 1;
+SELECT order_game.order_id, order_game.game_id, order_game."orderQuantity", user_id, "orderDate", status
+FROM public.order_game JOIN public.order ON order_game.order_id = id WHERE user_id = 1;
