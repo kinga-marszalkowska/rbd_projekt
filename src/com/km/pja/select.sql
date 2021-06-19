66a4e394a91aa63406b5d81f7a977fc975ff3cd7
marekk3301 <55376943+marekk3301@users.noreply.github.com>
2021-06-19 22:56:58 +0200

    total price

--- src/com/km/pja/select.sql
+++ src/com/km/pja/select.sql
@@ -7,10 +7,10 @@ FROM public.order_game JOIN public.order ON order_game.order_id = id WHERE user_
 SELECT (SELECT title FROM public.game WHERE id = category_game.game_id)
 FROM public.category_game JOIN public.category ON category_id = id WHERE category.name = 'zręcznościowa';
 
--- total calego zamowienia
-
--- newest games
-SELECT title, "releaseDate" FROM game ORDER BY "releaseDate" DESC;
-
--- most popular games
-SELECT game_id, SUM("orderQuantity") FROM public.order_game GROUP BY game_id;
+-- shows total order price --
+SELECT public.order.id, SUM(public.storage.price) AS cena
+from public.order 
+inner join public.order_game on public.order.id = public.order_game.order_id
+inner join public.storage on public.order_game.game_id = public.storage.game_id
+group by public.order.id
+order by public.order.id;
