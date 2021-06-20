a9e9b56f3ab238c78df5b8e54b7dda3aa2983c89
marekk3301 <55376943+marekk3301@users.noreply.github.com>
2021-06-20 19:18:16 +0200

    removed views, triggers, procedures and functions

--- src/com/km/pja/allInOne.sql
+++ src/com/km/pja/allInOne.sql
@@ -1049,107 +1049,6 @@ insert into public.storage (id, game_id, quantity, price, archived) values (9, 9
 insert into public.storage (id, game_id, quantity, price, archived) values (10, 10, 4, 75.71, false);
 
 
---- VIEWS --- views.sql ---
-
-
-CREATE VIEW newest
-AS
-SELECT title, "releaseDate" FROM public.game ORDER BY "releaseDate" DESC;
-
-CREATE VIEW most_popular
-AS
-SELECT game_id, SUM("orderQuantity") FROM public.order_game GROUP BY game_id;
-
-CREATE VIEW game_authors
-AS
-SELECT public.game.title, public.author.name, public.author.surname
-    from public.game
-    inner join public.author_game ON public.game.id = public.author_game.game_id
-    inner join public.author ON public.author_game.author_id = public.author.id;
-
-CREATE VIEW game_illustrators
-AS
-SELECT public.game.title, public.illustrator.name, public.illustrator.surname
-    from public.game
-    inner join public.illustrator_game ON public.game.id = public.illustrator_game.game_id
-    inner join public.illustrator ON public.illustrator_game.illustrator_id = public.illustrator.id;
-
-CREATE VIEW game_publishers
-AS
-SELECT public.game.title, public.publisher.name
-    from public.game
-    join public.publisher ON public.game.publisher_id = public.publisher.id;
-    
-
---- TRIGGERS --- triggers.sql ---
-    
-    
-CREATE TRIGGER cancel_orders_with_archived_games
-    AFTER UPDATE ON storage
-    FOR EACH ROW
-EXECUTE FUNCTION cancel_orders();
-
-CREATE TRIGGER subtract_quantity_order
-    AFTER INSERT ON order_game
-    FOR EACH ROW
-EXECUTE FUNCTION subtract_quantity();
-
-CREATE TRIGGER are_enough_copies_in_storage
-    BEFORE INSERT ON order_game
-    FOR EACH ROW
-EXECUTE FUNCTION copies_in_storage_check();
-
-
---- PROCEDURES AND FUNCTIONS --- procedures.sql ---
-
-
--- inserts new order with given params
-CREATE OR REPLACE PROCEDURE place_order(game_id int, user_id int, quantity int)
-    LANGUAGE sql
-AS $$
-    INSERT INTO public.order (id, user_id, "orderDate", status) VALUES ( ((SELECT MAX(id) FROM public.order) + 1), user_id, NOW(), 'ordered');
-    INSERT INTO public.order_game (order_id, game_id, "orderQuantity") values ((SELECT MAX(id) FROM public.order), game_id, quantity);
-$$;
-
--- cancel all uncompleted orders containing archived games
-CREATE OR REPLACE FUNCTION cancel_orders()
-    RETURNS TRIGGER
-    LANGUAGE 'plpgsql'
-AS $$
-BEGIN
-    IF NEW.archived THEN
-        UPDATE public.order SET status = 'canceled' WHERE (status = 'paid' OR status = 'ordered')
-                                                      AND id IN (SELECT order_game.order_id FROM public.order_game WHERE order_game.game_id = NEW.game_id);
-    END IF;
-    RETURN NULL;
-END
-$$;
-
--- subtract quantity when a new game is ordered
-CREATE OR REPLACE FUNCTION subtract_quantity()
-    RETURNS TRIGGER
-    LANGUAGE 'plpgsql'
-AS $$
-BEGIN
-    UPDATE storage SET quantity = ((SELECT quantity FROM public.storage WHERE game_id = NEW.game_id) - NEW."orderQuantity") WHERE game_id = NEW.game_id;
-    RETURN NULL;
-END
-$$;
-
--- prevent user form placing order if not enough copies are available
-CREATE OR REPLACE FUNCTION copies_in_storage_check()
-    RETURNS TRIGGER
-    LANGUAGE 'plpgsql'
-AS $$
-BEGIN
-    IF (SELECT quantity FROM public.storage WHERE game_id = NEW.game_id) < (NEW."orderQuantity") THEN
-        RAISE EXCEPTION 'Not enough copies in storage';
-    END IF;
-    RETURN NEW;
-END
-$$;
-
-
 --- ROLES --- roles.sql ---
 
 
@@ -1174,7 +1073,7 @@ GRANT INSERT, SELECT, UPDATE
 
 CREATE ROLE sales;
 GRANT INSERT, SELECT, UPDATE
-    ON public.games, public.awards
+    ON public.game, public.award
     TO sales;
 
 CREATE ROLE ecommerce;
