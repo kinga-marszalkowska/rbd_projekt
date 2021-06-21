04c49080221ed0558e89b92b2d6221d850a04e72
kinga-marszalkowska <kinga.marszalkowska72@gmail.com>
2021-06-21 12:36:17 +0200

    - corrected select for order total - added more select examples - added more update examples

--- src/com/km/pja/selects_example.sql
+++ src/com/km/pja/selects_example.sql
@@ -1,16 +1,36 @@
--- get 1st user's order history
+-- get 11th user's order history
 SELECT order_game.order_id, (SELECT title FROM public.game WHERE id = order_game.game_id),
        order_game."orderQuantity", user_id, "orderDate", status
-FROM public.order_game JOIN public.order ON order_game.order_id = id WHERE user_id = 11 ORDER BY "orderDate" ASC;
+FROM public.order_game JOIN public.order ON order_game.order_id = id
+WHERE user_id = 11
+ORDER BY "orderDate" ASC;
 
 -- select games by category name
 SELECT (SELECT title FROM public.game WHERE id = category_game.game_id)
-FROM public.category_game JOIN public.category ON category_id = id WHERE category.name = 'zręcznościowa';
+FROM public.category_game JOIN public.category ON category_id = id
+WHERE category.name = 'zręcznościowa';
 
--- shows total order price --
-SELECT public.order.id, SUM(public.storage.price) AS cena
-from public.order 
-inner join public.order_game on public.order.id = public.order_game.order_id
-inner join public.storage on public.order_game.game_id = public.storage.game_id
-group by public.order.id
-order by public.order.id;
+-- or multiple categories
+SELECT DISTINCT
+(SELECT title FROM public.game WHERE id = category_game.game_id)
+FROM public.category_game JOIN public.category ON category_id = id
+WHERE category.name IN ('zręcznościowa', 'karciana', 'edukacyjna');
+
+-- find games for 2 players
+SELECT * FROM public.game WHERE 2 BETWEEN "playersMin" and "playersMax";
+
+-- find games for kids of age > 7 published by rebel
+SELECT * FROM public.game
+WHERE (10 >= "ageMin") AND (game.publisher_id = (SELECT id FROM publisher WHERE name = 'rebel' )) ;
+
+-- shows totals of all orders
+SELECT public.order.id,
+       SUM(("orderQuantity" * (SELECT price FROM public.storage WHERE public.storage.game_id = public.order_game.game_id))) AS total
+FROM public.order JOIN public.order_game ON public.order.id = order_id
+GROUP BY public.order.id ;
+
+-- select games that need restocking (quantity is < 5)
+SELECT game_id, quantity, price, archived, (SELECT game.title FROM public.game WHERE id = public.storage.game_id)
+FROM public.storage
+WHERE quantity < 5
+ORDER BY public.storage.game_id;
--- src/com/km/pja/updates_example.sql
+++ src/com/km/pja/updates_example.sql
@@ -1,4 +1,12 @@
 -- archive game with id = 1
 UPDATE public.storage SET archived = true WHERE game_id = 1;
+-- restock game with id = 1 (add 10 new copies)
+UPDATE public.storage SET quantity = (SELECT quantity FROM public.storage WHERE game_id = 1) + 10 WHERE game.id = 1;
+
 -- change order status
 UPDATE public.order SET status = 'shipped' WHERE id = 1;
+
+-- change password
+UPDATE public.user SET password = '18QbPaFctsaAuG1qsx2g9P46y9K4KDfepr' WHERE id = 1;
+-- change user address
+UPDATE public.user SET country = 'Poland', city = 'Główczyce', "adressLine1" = '340 Almo Point', "postalCode" = '76-221' WHERE id = 1;
