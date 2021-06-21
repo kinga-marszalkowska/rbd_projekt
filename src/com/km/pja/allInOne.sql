56199a5def2a1d9c0f0f7435cd3cae659cf08f17
marekk3301 <55376943+marekk3301@users.noreply.github.com>
2021-06-21 17:13:41 +0200

    added datestyle setting again

--- src/com/km/pja/allInOne.sql
+++ src/com/km/pja/allInOne.sql
@@ -31,6 +31,7 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+SET datestyle = 'iso, dmy';
 
 SET default_tablespace = '';
 
