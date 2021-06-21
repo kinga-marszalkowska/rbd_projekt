f0ff730a768123fb8974ddff52878514c7854ea6
marekk3301 <55376943+marekk3301@users.noreply.github.com>
2021-06-21 23:51:47 +0200

    added views to allInOne

--- src/com/km/pja/allInOne.sql
+++ src/com/km/pja/allInOne.sql
@@ -1051,6 +1051,44 @@ insert into public.storage (id, game_id, quantity, price, archived) values (9, 9
 insert into public.storage (id, game_id, quantity, price, archived) values (10, 10, 4, 75.71, false);
 
 
+--- VIEWS --- views.sql ---
+
+
+CREATE VIEW public.newest
+AS
+SELECT title, "releaseDate" FROM public.game ORDER BY "releaseDate" DESC;
+
+CREATE VIEW public.most_popular
+AS
+SELECT game_id, SUM("orderQuantity") FROM public.order_game GROUP BY game_id;
+
+CREATE VIEW public.game_authors
+AS
+SELECT public.game.title, public.author.name, public.author.surname
+    from public.game
+    inner join public.author_game ON public.game.id = public.author_game.game_id
+    inner join public.author ON public.author_game.author_id = public.author.id;
+
+CREATE VIEW public.game_illustrators
+AS
+SELECT public.game.title, public.illustrator.name, public.illustrator.surname
+    from public.game
+    inner join public.illustrator_game ON public.game.id = public.illustrator_game.game_id
+    inner join public.illustrator ON public.illustrator_game.illustrator_id = public.illustrator.id;
+
+CREATE VIEW public.game_publishers
+AS
+SELECT public.game.title, public.publisher.name
+    from public.game
+    join public.publisher ON public.game.publisher_id = public.publisher.id;
+    
+
+--- TRIGGERS --- triggers.sql ---
+
+
+
+
+
 --- ROLES --- roles.sql ---
 
 
